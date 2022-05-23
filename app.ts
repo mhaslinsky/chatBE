f8b8b290f21f237156a921c4ef30923a3960e72d
mhaslinsky <mhaslinsky@gmail.com>
2022-05-23 01:25:06 -0400

    added handler for room join requests, and cb to send back number of connected sockets

--- app.ts
+++ app.ts
@@ -20,5 +20,14 @@ namespaces.forEach((ns) => {
   io.of(ns.endpoint).on("connection", (socket) => {
     // console.log(`${socket.id} has joined ${ns.endpoint}!`);
     socket.emit("nsRoomLoad", ns.rooms);
+    socket.on("joinRoom", async (roomToJoin, numberOfUsersCallback) => {
+      socket.join(roomToJoin);
+      //returns set of all sockets connected to room in ns
+      const ids = await io.of(ns.endpoint).in(roomToJoin).allSockets();
+      console.log(ids);
+      const numOfUsersConnected = ids.size;
+
+      numberOfUsersCallback(numOfUsersConnected);
+    });
   });
 });
