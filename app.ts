bb4b39339f92bd1ff3db70e5acffff2652c41ff5
mhaslinsky <mhaslinsky@gmail.com>
2022-05-25 00:43:42 -0400

    added leave room handler to fix left open sockets causing duplicate sessions

--- app.ts
+++ app.ts
@@ -20,14 +20,21 @@ namespaces.forEach((ns) => {
   io.of(ns.endpoint).on("connection", (socket) => {
     // console.log(`${socket.id} has joined ${ns.endpoint}!`);
     socket.emit("nsRoomLoad", ns.rooms);
+    //when someone emits a join request, we take the room they want to join
+    //and pass back the number of users in it
     socket.on("joinRoom", async (roomToJoin, numberOfUsersCallback) => {
       socket.join(roomToJoin);
       //returns set of all sockets connected to room in ns
       const ids = await io.of(ns.endpoint).in(roomToJoin).allSockets();
-      console.log(ids);
       const numOfUsersConnected = ids.size;
-
       numberOfUsersCallback(numOfUsersConnected);
+    });
+    socket.on("leaveRoom", (roomToLeave) => {
+      socket.leave(roomToLeave);
+    });
+    socket.on("newMessageToServer", (msg) => {
+      const roomTitle = Object.keys(socket.rooms)[1];
+      io.of(ns.endpoint).to(roomTitle).emit("messageToClients", msg);
     });
   });
 });
