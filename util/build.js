5a96b768fb2e7f42e4a670376afb8d48a9897102
mhaslinsky <mhaslinsky@gmail.com>
2022-06-10 23:42:05 -0400

    build process

---
+++ util/build.js
@@ -0,0 +1,9 @@
+const fs = require("fs-extra");
+const childProcess = require("child_process");
+
+try {
+  childProcess.exec("tsc --build tsconfig.json");
+  // fs.copySync(`./nginx`, `./`);
+} catch (err) {
+  console.log(err);
+}
